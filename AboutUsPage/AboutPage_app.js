ec963aff91ca40549adc9a7715fb9915e14346ee
Dmytro <pelexov.mitya@gmail.com>
2024-11-26 18:35:24 +0200

    finished 768px adaptation AboutUsPage

--- AboutUsPage/AboutPage_app.js
+++ AboutUsPage/AboutPage_app.js
@@ -10,12 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
   // виїжджаюче меню та зміна кнопки моб.меню
   let navbarMenuBtn = document.querySelector(".nav-menu");
   let mobMenu = document.querySelector(".mob-menu");
-  let bodyWrapper = document.querySelector("body");
+  let body = document.querySelector("body");
 
   navbarMenuBtn.addEventListener("click", function () {
     navbarMenuBtn.classList.toggle("active");
     mobMenu.classList.toggle("active");
-    bodyWrapper.classList.toggle("stop-scroll");
+    body.classList.toggle("stop-scroll");
   });
 
   //  mob-menu accordion
@@ -45,6 +45,22 @@ document.addEventListener("DOMContentLoaded", function () {
     })
   );
 
+  // our-staff slider
+  $(".our-staff-cards").slick({
+    slidesToShow: 2,
+    arrows: false,
+  });
+
+  // зміна вигляду our-staff  card
+  let ourStaffCards = document.querySelectorAll(".st-card");
+
+  ourStaffCards.forEach((item) =>
+    item.addEventListener("click", function () {
+      ourStaffCards.forEach((el) => el.classList.remove("active"));
+      item.classList.add("active");
+    })
+  );
+
   // logos slider
   let logosSwiper = new Swiper(".logos-slider", {
     slidesPerView: 2,
@@ -52,5 +68,95 @@ document.addEventListener("DOMContentLoaded", function () {
     loop: true,
     watchOverflow: true,
     spaceBetween: 30,
+
+    breakpoints: {
+      768: { slidesPerView: 3, slidesPerGroup: 1 },
+    },
   });
+
+  // відправка форми -------------------
+  let footerForm = document.querySelector(".subscribe-form");
+
+  footerForm.addEventListener("submit", formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+    // перевірка заповненості полів
+    let error = formValidate(footerForm);
+    // отримання даних з полей
+    let footerFormData = new FormData(footerForm);
+
+    if (error === 0) {
+      body.classList.add("sending-form", "stop-scroll");
+
+      // запрос на сервер
+      let response = await fetch(
+        "https://my-json-server.typicode.com/kznkv-skillup/server/orders",
+        {
+          method: "POST",
+          body: footerFormData,
+        }
+      );
+
+      if (response.ok) {
+        let result = await response.json();
+        alert("All right!");
+        footerForm.reset();
+        // очистка форми після відправки
+        body.classList.remove("sending-form", "stop-scroll");
+      } else {
+        alert("Not sending!");
+        body.classList.remove("sending-form", "stop-scroll");
+      }
+    } else {
+      // якщо є проблема з валідацією
+      alert("All fields required!");
+    }
+  }
+
+  function formValidate(footerForm) {
+    let error = 0;
+
+    let formRequired = document.querySelectorAll(".required");
+
+    for (let i = 0; i < formRequired.length; i++) {
+      const input = formRequired[i];
+      // кожен елемент масиву помістили в змінну input
+      formRemoveError(input);
+      // у кожного елемента видаляємо клас "error"
+
+      if (input.classList.contains("input-email")) {
+        if (emailTest(input)) {
+          // якщо пошту введено неправильно
+          formAddError(input);
+          // додаємо елементу "error"
+          error++;
+        } else {
+          if (input.value === "") {
+            // якщо поле незаповнено
+            formAddError(input);
+            // додаємо елементу "error"
+            error++;
+          }
+        }
+      }
+    }
+
+    return error;
+    // значення буде або 0 або ні
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add("error");
+    input.classList.add("error");
+  }
+  function formRemoveError(input) {
+    input.parentElement.classList.remove("error");
+    input.classList.remove("error");
+  }
+
+  //перевірка правильного формату пошти
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+  }
 });
